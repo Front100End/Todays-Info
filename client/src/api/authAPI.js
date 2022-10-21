315ecbb4c6823376a5ebc2d65ce8cb4a14f47dc9
seongjin cho <aktps12@gmail.com>
2022-10-21 15:31:30 +0900

    v17 update: HomeHeader mobile UI, Modify Join guide bug

--- client/src/api/authAPI.js
+++ client/src/api/authAPI.js
@@ -1,21 +1,6 @@
 import axios from "axios";
-import { setUser } from "../modules/userReducer";
 
-export const authRequest = async (accessCheck, dispatch) => {
-  try {
-    const res = await axios.get("https://todays-info.site/api/users/auth", {
-      withCredentials: true,
-    });
-    if (res.data.isAuth === true) {
-      accessCheck(res.data.isAuth, dispatch(setUser(res.data)));
-    } else {
-      return;
-    }
-  } catch (err) {
-    return err;
-  }
-};
-
-//api 로 결과 값을 isAuth를 받아왔는데 true이다?
-//그러면 dispatch로 user데이터 저장해주고
-//Home화면으로 이동 시키는 것.
+export const authRequest = async () =>
+  axios.get("https://todays-info.site/api/users/auth", {
+    withCredentials: true,
+  });
